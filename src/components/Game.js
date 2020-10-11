146961b68d4141e4e309ae102187a1979d886cb5
Andrey-Sushkevich <64629724+Andrey-Sushkevich@users.noreply.github.com>
2020-10-11 15:05:28 +0300

    Add files via upload

--- src/components/Game.js
+++ src/components/Game.js
@@ -15,3 +15,54 @@ export default class Game extends React.Component{
         )
     }
 }
+
+class GameInterface {
+
+  getUserDataFromInputs() {
+        const players = document.getElementById("user_form").elements;
+        const userData = {
+            "first_name": players.first_name.value,
+            "last_name":players.last_name.value,
+            "email": players.email.value
+        }
+        return userData;
+    }
+
+  saveLastLoginData(userData, name_localStorage) {
+        localStorage.setItem(name_localStorage, JSON.stringify(userData));
+    }
+/*сохранение в Local storage*/
+
+  saveUserData(userData, name_localStorage) {
+        let dataBase = null;
+        if (!localStorage.getItem(name_localStorage)) {
+            dataBase = [];
+        }
+        else {
+            dataBase = JSON.parse(localStorage.getItem(name_localStorage));
+        }
+        if (GameInterface.isUnique(userData, dataBase)) {
+            dataBase.push(userData);
+            localStorage.setItem(name_localStorage, JSON.stringify(dataBase));
+        }
+    }
+
+isUnique(obj, arrOfObjs) {
+        for (let i = 0; i < arrOfObjs.length; i++) {
+            if (JSON.stringify(obj) === JSON.stringify(arrOfObjs[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+/*получение из  Local storage*/
+loadLastUserData() {
+        if (localStorage.getItem("lastLoginUserData")) {
+            const lastUserData = JSON.parse(localStorage.getItem("lastLoginUserData"));
+            const players = document.getElementById("user_form").elements;
+            players.first_name.value = lastUserData.first_name;
+            players.last_name.value = lastUserData.last_name;
+            players.email.value = lastUserData.email;
+        }
+    }
+}
